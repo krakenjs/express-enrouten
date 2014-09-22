69a6883542f0419612eaa929535b1312c21fa3dc
Christopher Watford <christopher.watford@gmail.com>
2014-09-22 11:10:11 -0400

    Missed a spot to pass routerOptions

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function mount(app, options) {
 
         if (typeof options.directory === 'string') {
             options.directory = resolve(options.basedir, options.directory);
-            directory(router, options.directory);
+            directory(router, options.directory, routerOptions);
         }
 
         if (typeof options.routes === 'object') {

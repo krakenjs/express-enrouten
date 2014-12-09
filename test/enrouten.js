57dec7ed93313d5e974601c662fc76c888656c4b
Poornima Venkat <pvenkatakrishnan@paypal.com>
2014-12-09 09:01:51 -0800

    Merge router options.

--- test/enrouten.js
+++ test/enrouten.js
@@ -237,7 +237,7 @@ function run(test, name, mount, fn) {
 
 
         t.test('named', function (t) {
-            var app, settings
+            var app, settings;
 
             app = express();
             settings = {
@@ -341,7 +341,7 @@ function run(test, name, mount, fn) {
 
 
         t.test('named', function (t) {
-            var app, settings
+            var app, settings;
 
             app = express();
             settings = {
@@ -510,8 +510,119 @@ function run(test, name, mount, fn) {
             t.end();
         });
 
-    });
+        t.test('single middleware', function(t) {
+            var app, settings;
 
+            app = express();
+            settings = {
+                routes: [
+                    {
+                        path: '/',
+                        method: 'get',
+                        middleware: [
+                            function(req, res, next) {
+                                res.value = 'middleware';
+                                next();
+                            }
+                        ],
+                        handler: function (req, res) {
+                            res.send(res.value);
+                        }
+                    }
+                ]
+            };
+
+            fn(app, settings);
+
+            request(app)
+                .get(mount)
+                .expect('Content-Type', /html/)
+                .expect(200, 'middleware', function (err) {
+                    t.error(err);
+                    t.end();
+                });
+        });
+
+        t.test('multiple middleware', function(t) {
+            var app, settings;
+
+            app = express();
+            settings = {
+                routes: [
+                    {
+                        path: '/',
+                        method: 'get',
+                        middleware: [
+                            function(req, res, next) {
+                                res.value1 = 1;
+                                next();
+                            },
+                            function(req, res, next) {
+                                res.value2 = 2;
+                                next();
+                            }
+                        ],
+                        handler: function (req, res) {
+                            res.send((res.value1 + res.value2).toString());
+                        }
+                    }
+                ]
+            };
+
+            fn(app, settings);
+
+            request(app)
+                .get(mount)
+                .expect('Content-Type', /html/)
+                .expect(200, (3).toString(), function (err) {
+                    t.error(err);
+                    t.end();
+                });
+        });
+
+        t.test('error thrown in middleware', function(t) {
+            var app, settings;
+
+            app = express();
+            settings = {
+                routes: [
+                    {
+                        path: '/',
+                        method: 'get',
+                        middleware: [
+                            function(req, res, next) {
+                                next(new Error('middleware error'));
+                            },
+                            function(req, res, next) {
+                                res.msg = 'You wont see this';
+                                next();
+                            }
+                        ],
+                        handler: function (req, res) {
+                            res.send(res.msg);
+                        }
+                    }
+                ]
+            };
+
+            fn(app, settings);
+
+            // error handler
+            app.use(function(err, req, res, next) {
+                res.status(503).send(err.message);
+            });
+
+            request(app)
+                .get(mount)
+                .expect('Content-Type', /html/)
+                .expect(503, 'middleware error', function (err) {
+                    t.error(err);
+                    t.end();
+                });
+        });
+
+
+    });
 
     test('path generation', function (t) {
 
@@ -563,6 +674,8 @@ function run(test, name, mount, fn) {
 
             t.end();
         });
+
+        t.test()
 
     });
 
